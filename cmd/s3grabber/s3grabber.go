e35df6b80d0064c32fe2133f7480a1e4536362da
Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
2022-01-31 16:14:37 +0200

    *: add docs, e2e tests
    Add a bit of documentation about how it all works. Add e2e tests with
    Docker.
    
    ```
    Building tester
    Step 1/6 : FROM golang:1.17
     ---> 80d9a75ccb38
    Step 2/6 : WORKDIR /code
     ---> Using cache
     ---> d7a1d27a8c10
    Step 3/6 : COPY go.mod go.sum ./
     ---> Using cache
     ---> ad6b6a25df10
    Step 4/6 : RUN go mod download
     ---> Using cache
     ---> 18963effdd23
    Step 5/6 : COPY . /code
     ---> b4a64a977733
    Step 6/6 : RUN go build -o s3grabber ./cmd/s3grabber
     ---> Running in 8e286fbe107b
    Removing intermediate container 8e286fbe107b
     ---> 00b23838424b
    Successfully built 00b23838424b
    Successfully tagged s3grabber_tester:latest
    Stopping s3grabber_minio1_1 ... done
    Stopping s3grabber_minio2_1 ... done
    Removing s3grabber_tester_1 ... done
    Removing s3grabber_minio1_1 ... done
    Removing s3grabber_minio2_1 ... done
    Removing network s3grabber_test
    Creating network "s3grabber_test" with the default driver
    Creating s3grabber_minio1_1 ... done
    Creating s3grabber_minio2_1 ... done
    Creating s3grabber_tester_1 ... done
    ?       github.com/vinted/S3Grabber/cmd/s3grabber       [no test files]
    ok      github.com/vinted/S3Grabber/internal/cfg        0.002s
    ok      github.com/vinted/S3Grabber/internal/downloader 1.088s
    ?       github.com/vinted/S3Grabber/internal/installer  [no test files]
    ?       github.com/vinted/S3Grabber/internal/s3grabber  [no test files]
    ok      github.com/vinted/S3Grabber/test        1.037s
    ```

--- cmd/s3grabber/s3grabber.go
+++ cmd/s3grabber/s3grabber.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"context"
 	"os"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
-	"github.com/oklog/run"
 	"github.com/vinted/S3Grabber/internal/cfg"
-	"github.com/vinted/S3Grabber/internal/downloader"
-	"github.com/vinted/S3Grabber/internal/installer"
+	"github.com/vinted/S3Grabber/internal/s3grabber"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-// InitializeLogger initializes a logger with the given parameters.
-func InitializeLogger(logFormat string, logLevel string) log.Logger {
+// initializeLogger initializes a logger with the given parameters.
+func initializeLogger(logFormat string, logLevel string) log.Logger {
 	var logger log.Logger
 
 	switch logFormat {
@@ -45,51 +42,14 @@ func main() {
 
 	kingpin.Parse()
 
-	logger := InitializeLogger(*logFormat, *logLevel)
+	logger := initializeLogger(*logFormat, *logLevel)
 	cfg, err := cfg.ReadConfig(*configFile)
 	if err != nil {
 		_ = level.Error(logger).Log("msg", "failed to read config file", "path", *configFile, "err", err.Error())
 		os.Exit(1)
 	}
 
-	runS3Grabber(logger, cfg)
-}
-
-func runS3Grabber(logger log.Logger, config cfg.GlobalConfig) {
-	installers := make([]*installer.Installer, 0, len(config.Grabbers))
-	for grabberName, grabber := range config.Grabbers {
-		bucketCfgs := []cfg.BucketConfig{}
-		for _, bktName := range grabber.Buckets {
-			bkt, ok := config.Buckets[bktName]
-			if !ok {
-				_ = level.Error(logger).Log("msg", "failed to find bucket", "bucket_name", bktName, "grabber_name", grabberName)
-				os.Exit(1)
-			}
-			bucketCfgs = append(bucketCfgs, bkt)
-		}
-
-		bm, err := downloader.NewBucketManager(bucketCfgs)
-		if err != nil {
-			_ = level.Error(logger).Log("msg", "failed to construct a new bucket manager", "grabber_name", grabberName)
-		}
-
-		installers = append(installers, installer.NewInstaller(bm, grabber.Commands, grabber.File, grabber.Path, config.Shell))
-	}
-
-	g := &run.Group{}
-	ctx, cancel := context.WithTimeout(context.Background(), config.Timeout)
-	defer cancel()
-
-	for _, i := range installers {
-		g.Add(func() error {
-			return i.Install(ctx)
-		}, func(error) {
-			cancel()
-		})
-	}
-
-	if err := g.Run(); err != nil {
-		_ = level.Error(logger).Log("failed to run grabber(-s)", "err", err.Error())
+	if err := s3grabber.RunS3Grabber(logger, cfg); err != nil {
 		os.Exit(1)
 	}
 }
---
+++ test/run_test.go
@@ -0,0 +1,85 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/stretchr/testify/require"
+	"github.com/vinted/S3Grabber/internal/cfg"
+	"github.com/vinted/S3Grabber/internal/downloader"
+	"github.com/vinted/S3Grabber/internal/s3grabber"
+)
+
+func TestS3GrabberMain(t *testing.T) {
+	t.Parallel()
+
+	tmpDir := filepath.Join(os.TempDir(), "s3grabber")
+	require.Nil(t, os.MkdirAll(tmpDir, os.ModePerm))
+	t.Cleanup(func() {
+		require.Nil(t, os.RemoveAll(tmpDir))
+	})
+
+	grabberCfg := cfg.GlobalConfig{
+		Buckets: map[string]cfg.BucketConfig{
+			"test1": {
+				Host:      "minio1:9000",
+				AccessKey: "foobar",
+				SecretKey: "foobardd",
+				Bucket:    "test",
+			},
+			"test2": {
+				Host:      "minio2:9000",
+				AccessKey: "foobar",
+				SecretKey: "foobardd",
+				Bucket:    "test",
+			},
+		},
+		Grabbers: map[string]cfg.GrabberConfig{
+			"testing": {
+				Buckets:  []string{"test1", "test2"},
+				File:     "example.tar.gz",
+				Path:     tmpDir,
+				Commands: []string{fmt.Sprintf("echo foobar > %s", filepath.Join(tmpDir, "somefile"))},
+			},
+		},
+		Timeout: 5 * time.Second,
+		Shell:   "/bin/sh",
+	}
+	err := s3grabber.RunS3Grabber(log.NewLogfmtLogger(os.Stderr), grabberCfg)
+	require.NotNil(t, err)
+
+	// Upload the file to both buckets.
+	time.Sleep(1 * time.Second) // To ensure ctime < modify time.
+	bm, err := downloader.NewBucketManager([]cfg.BucketConfig{
+		grabberCfg.Buckets["test1"], grabberCfg.Buckets["test2"],
+	})
+	require.Nil(t, err)
+	// Only upload to one bucket to check whether it works properly.
+	require.Nil(t, bm.CreateBucket(context.Background(), "test", 0))
+	require.Nil(t, bm.CreateBucket(context.Background(), "test", 1))
+	require.Nil(t, bm.PutFile(context.Background(), "../internal/downloader/example.tar.gz", "/example.tar.gz", 1))
+
+	err = s3grabber.RunS3Grabber(log.NewLogfmtLogger(os.Stderr), grabberCfg)
+	require.Nil(t, err)
+
+	checkFileContentEqual(t, filepath.Join(tmpDir, "test"), "Hello world!\n")
+	checkFileContentEqual(t, filepath.Join(tmpDir, "somefile"), "foobar\n")
+}
+
+func checkFileContentEqual(t *testing.T, path, content string) {
+	f, err := os.Open(path)
+	require.Nil(t, err)
+	t.Cleanup(func() {
+		f.Close()
+	})
+	fileContent, err := io.ReadAll(f)
+	require.Nil(t, err)
+	require.Equal(t, string(fileContent), string(content))
+
+}
